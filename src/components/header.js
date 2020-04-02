a26f445d7420caaee8518da03ad65dca7a99e6ee
AmoDinho <u14284783@tuks.co.za>
2020-04-02 20:26:48 +0200

    sorting out layout

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header className="border-top bg-transparent">
     <div
       style={{
         margin: `0 auto`,
@@ -16,11 +11,10 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 className="text-red font-display text-xl3">
         <Link
           to="/"
           style={{
-            color: `white`,
             textDecoration: `none`,
           }}
         >
--- src/components/layout.js
+++ src/components/layout.js
@@ -34,11 +34,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <footer></footer>
       </div>
     </>
   )
