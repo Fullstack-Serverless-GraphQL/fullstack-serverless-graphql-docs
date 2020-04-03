dd2d7ecf7f7e33c54486147c28929774bc4fc1a4
AmoDinho <u14284783@tuks.co.za>
2020-04-03 18:01:39 +0200

    made nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,9 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header className="border-top bg-transparent">
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 className="text-red font-display text-xl3">
+  <header className="border-top bg-transparent flex items-center justify-between flex-wrap p-6">
+    <div className="flex items-center flex-no-shrink text">
+      <h1 className="text-red font-bold font-display lg:text-3xl s:text-xl">
         <Link
           to="/"
           style={{
