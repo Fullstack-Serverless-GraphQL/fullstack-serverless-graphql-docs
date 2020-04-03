01f70cb659502ca4d4d1700baac4508f4bd08ede
AmoDinho <u14284783@tuks.co.za>
2020-04-03 18:18:19 +0200

    added the copy text

--- src/components/landing/hero.js
+++ src/components/landing/hero.js
@@ -1,10 +1,20 @@
 import React from "react"
 import HeadingOne from "../typography/HeadingOne"
+import BodyOne from "../typography/BodyOne"
+
 const Hero = () => (
   <>
     <div className="flex lg:flex-row s:flex-col">
       <div>
         <HeadingOne>Build a fullstack serverless GraphQL App!</HeadingOne>
+        <BodyOne>
+          The way APIs are built and the tools that are used to consume them
+          have have changed drastically over the years. This free resource is
+          your chance to learn how to build a GraphQL API in a serverless
+          environment on AWS Lambda and consume it on a Vue.JS frontend based on
+          a design system using Apollo. Everything you learn here will not be a
+          waste of time, it is 100% reproducible for production use cases.{" "}
+        </BodyOne>
       </div>
     </div>
   </>
