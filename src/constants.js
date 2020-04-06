2b974c0263a6dd8d8e07f588dcc9f1171caf4daa
AmoDinho <u14284783@tuks.co.za>
2020-04-06 22:31:52 +0200

    added frontend stuff

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,34 @@
 import VueLogo from "./images/vue.svg"
+import ApolloLogo from "./images/apollo.svg"
+import AntLogo from "./images/ant.jpg"
+import Cognitologo from "./images/aws-cognito.svg"
+import DynamoLogo from "./images/aws-lambda.svg"
+import LambdaLogo from "./images/aws-lambda.svg"
+import PostmarkLogo from "./images/postmark.jpeg"
+import StripLogo from "./images/stripe.svg"
+import TailwindLogo from "./images/tailwind.png"
 const techStack = [
   {
     logo: VueLogo,
     technology: "Vue",
     url: "https://vuejs.org/",
   },
+  {
+    logo: ApolloLogo,
+    technology: "Vue Apollo",
+    url: "https://vue-apollo.netlify.com/",
+  },
+  {
+    logo: TailwindLogo,
+    technology: "Vue",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    logo: AntLogo,
+    technology: "Ant Design",
+    url: "https://www.antdv.com/",
+  },
 ]
 
+const backendTechStack = [{}]
 export default { techStack }
