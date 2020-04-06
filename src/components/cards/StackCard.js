6a441710c20d9a1261d96bb56504e6bd008fd5ae
AmoDinho <u14284783@tuks.co.za>
2020-04-06 21:58:34 +0200

    added the constants file

--- src/components/cards/StackCard.js
+++ src/components/cards/StackCard.js
@@ -3,20 +3,29 @@ import React from "react"
 import HeadingThree from "../typography/HeadingThree"
 import BodyOne from "../typography/BodyOne"
 
-const StackCard = ({ Title, className, url }) => (
+const StackCard = ({ Title, className, url, linkURL }) => (
   <div className={`${className} cursor-pointer `}>
     <img src={url} alt="card logo" />
     <HeadingThree>{Title}</HeadingThree>
     <div className="flex flex-row">
-      <img src="../../images.launch.svg" />
-      <BodyOne>Docs</BodyOne>
+      <a
+        className="text-blue"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={linkURL}
+      >
+        <img src="../../images.launch.svg" />
+        <BodyOne>Docs</BodyOne>
+      </a>
     </div>
   </div>
 )
 
 StackCard.propTypes = {
-  children: PropTypes.string,
+  url: PropTypes.string,
   className: PropTypes.string,
+  linkURL: PropTypes.string,
+  Title: PropTypes.string,
 }
 
 StackCard.defaultProps = {
--- src/components/landing/TechStack.js
+++ src/components/landing/TechStack.js
@@ -1,6 +1,21 @@
 import React from "react"
 import BodyOne from "../typography/BodyOne"
-
-const TechStack = () => <></>
+import HeadingTwo from "../typography/HeadingTwo"
+import HeadingThree from "../typography/HeadingThree"
+import StackCard from "../cards/StackCard"
+import constants from "../../constants"
+const TechStack = () => (
+  <>
+    <div>
+      <HeadingTwo>Tech stack</HeadingTwo>
+      <HeadingThree>Front end</HeadingThree>
+      <div className="flex flex-row">
+        {constants.techStack.map(t => (
+          <StackCard url={t.logo} linkURL={t.url} Title={t.technology} />
+        ))}
+      </div>
+    </div>
+  </>
+)
 
 export default TechStack
