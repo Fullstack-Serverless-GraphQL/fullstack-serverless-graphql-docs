4b93301fd39bbe076706914a74172a7e7dd7cde7
AmoDinho <u14284783@tuks.co.za>
2020-04-09 18:58:41 +0200

    fixed logos

--- src/components/cards/StackCard.js
+++ src/components/cards/StackCard.js
@@ -7,7 +7,7 @@ const StackCard = ({ Title, className, url, linkURL }) => (
   <div
     className={`${className} cursor-pointer shadow-lg rounded-lg flex flex-col p-10 p- ml-10 h-200 max-w-3xl bg-white`}
   >
-    <img src={url} alt="card logo" className="h-10 mb-3" />
+    <img src={url} alt="card logo" className="h-16 mb-3" />
     <HeadingThree>{Title}</HeadingThree>
     <div className="flex flex-row ">
       <a
