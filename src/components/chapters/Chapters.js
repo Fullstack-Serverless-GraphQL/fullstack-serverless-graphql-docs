580b1286acf8db7103bcc346be64e09d4198809f
AmoDinho <u14284783@tuks.co.za>
2020-05-08 18:22:34 +0200

    fixed the setup of the infratsure posts

--- src/components/chapters/Chapters.js
+++ src/components/chapters/Chapters.js
@@ -7,30 +7,52 @@ const Chapters = ({
   data: {
     allMarkdownRemark: { edges },
   },
-}) => (
-  <>
-    <div className="mt-10">
-      <HeadingTwo className="text-center">Chapters</HeadingTwo>
-      {/* chAPTERS GRID */}
-      <div className="grid grid-cols-2">
-        <div className="mt-5">
-          <HeadingTwo className="">Epilouge</HeadingTwo>
+}) => {
+  const epilouges = edges.filter(i => i.node.frontmatter.part === "Epilouge")
+  const backendPosts = edges.filter(
+    i => i.node.frontmatter.part === "setting up backend"
+  )
+  return (
+    <>
+      <div className="mt-10">
+        <HeadingTwo className="text-center">Chapters</HeadingTwo>
+        {/* chAPTERS GRID */}
+        <div className="grid grid-cols-2">
+          <div className="mt-5">
+            {/* start of chapter one */}
+            <HeadingTwo className="">Epilouge</HeadingTwo>
 
-          {/* Start of chapter one */}
-          {edges.map(edge => (
-            <BodyOne className="text-yellow m-3" key={edge.node.id}>
-              <Link to={edge.node.frontmatter.path}>
-                {edge.node.frontmatter.title}
-              </Link>
-            </BodyOne>
-          ))}
+            {/* Start of chapter one */}
+            {epilouges.map(edge => (
+              <BodyOne className="text-yellow m-3" key={edge.node.id}>
+                <Link to={edge.node.frontmatter.path}>
+                  {edge.node.frontmatter.title}
+                </Link>
+              </BodyOne>
+            ))}
 
-          {/* end of chapter one */}
+            {/* end of chapter one */}
+          </div>
+
+          {/* start of chapter two */}
+          <div className="mt-5">
+            <HeadingTwo className="">Setting Up infrastructure</HeadingTwo>
+
+            {backendPosts.map(edge => (
+              <BodyOne className="text-yellow m-3" key={edge.node.id}>
+                <Link to={edge.node.frontmatter.path}>
+                  {edge.node.frontmatter.title}
+                </Link>
+              </BodyOne>
+            ))}
+          </div>
+
+          {/* end of chapter two */}
         </div>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default props => (
   <StaticQuery
@@ -42,6 +64,8 @@ export default props => (
               frontmatter {
                 path
                 title
+                part
+                chapter
               }
             }
           }
