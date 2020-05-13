7d1995d966d1aa1c80898a2ecd890acb5f49ea26
AmoDinho <u14284783@tuks.co.za>
2020-05-13 21:07:46 +0200

    added props

--- src/components/chapters/ChapterBox.js
+++ src/components/chapters/ChapterBox.js
@@ -1,7 +1,21 @@
 import React from "react"
 
-const ChapterBox = ({}) => {
-  return <></>
+const ChapterBox = ({ text, edges }) => {
+  return (
+    <>
+      <div className="mt-5">
+        <HeadingThree className="">{text}</HeadingThree>
+
+        {edges.map(edge => (
+          <BodyOne className="text-yellow m-3" key={edge.node.id}>
+            <Link to={edge.node.frontmatter.path}>
+              {edge.node.frontmatter.title}
+            </Link>
+          </BodyOne>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default ChapterBox
