5ae1407f835c5cad5c730acbdf370e9b3ca9727e
AmoDinho <u14284783@tuks.co.za>
2020-07-15 20:00:54 +0200

    removed antd

--- src/components/chapters/ChapterBox.js
+++ src/components/chapters/ChapterBox.js
@@ -1,22 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Collapse } from "antd"
 import HeadingThree from "../typography/HeadingThree"
 import BodyOne from "../typography/BodyOne"
 import PropTypes from "prop-types"
+import "antd/dist/antd.css"
+const { Panel } = Collapse
 
-const ChapterBox = ({ text, edges }) => {
+const ChapterBox = ({ text, edges, collapse }) => {
   return (
     <>
       <div className="mt-5">
-        <HeadingThree className="">{text}</HeadingThree>
+        {collapse ? (
+          <>
+            <HeadingThree className="">{text}</HeadingThree>
 
-        {edges.map(edge => (
-          <BodyOne className="text-yellow m-3" key={edge.node.id}>
-            <Link to={edge.node.frontmatter.path}>
-              {edge.node.frontmatter.title}
-            </Link>
-          </BodyOne>
-        ))}
+            {edges.map(edge => (
+              <BodyOne className="text-yellow m-3" key={edge.node.id}>
+                <Link to={edge.node.frontmatter.path}>
+                  {edge.node.frontmatter.title}
+                </Link>
+              </BodyOne>
+            ))}
+          </>
+        ) : (
+          <>
+            {" "}
+            <HeadingThree className="">{text}</HeadingThree>
+            {edges.map(edge => (
+              <BodyOne className="text-yellow m-3" key={edge.node.id}>
+                <Link to={edge.node.frontmatter.path}>
+                  {edge.node.frontmatter.title}
+                </Link>
+              </BodyOne>
+            ))}
+          </>
+        )}
       </div>
     </>
   )
