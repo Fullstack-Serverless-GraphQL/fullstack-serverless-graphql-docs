a3325299053c66e5c439338679e1bc5305a3ff89
AmoDinho <u14284783@tuks.co.za>
2020-07-15 20:25:15 +0200

    hide of the chapters is working

--- src/components/chapters/ChapterBox.js
+++ src/components/chapters/ChapterBox.js
@@ -1,13 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import { Collapse } from "antd"
 import HeadingThree from "../typography/HeadingThree"
 import BodyOne from "../typography/BodyOne"
 import PropTypes from "prop-types"
-import "antd/dist/antd.css"
-const { Panel } = Collapse
 
 const ChapterBox = ({ text, edges, collapse }) => {
+  const [open, setOpen] = useState(false)
+
+  const openChapters = () => {
+    console.log("hi")
+    setOpen(!open)
+  }
   return (
     <>
       <div className="mt-5">
@@ -26,14 +29,25 @@ const ChapterBox = ({ text, edges, collapse }) => {
         ) : (
           <>
             {" "}
-            <HeadingThree className="">{text}</HeadingThree>
-            {edges.map(edge => (
-              <BodyOne className="text-yellow m-3" key={edge.node.id}>
-                <Link to={edge.node.frontmatter.path}>
-                  {edge.node.frontmatter.title}
-                </Link>
-              </BodyOne>
-            ))}
+            <button
+              className="cursor-pointer"
+              onClick={() => openChapters() && console.log(open)}
+            >
+              {text}
+            </button>
+            <span role="chapter opener">
+              {open === true && (
+                <>
+                  {edges.map(edge => (
+                    <BodyOne className="text-yellow m-3" key={edge.node.id}>
+                      <Link to={edge.node.frontmatter.path}>
+                        {edge.node.frontmatter.title}
+                      </Link>
+                    </BodyOne>
+                  ))}
+                </>
+              )}
+            </span>
           </>
         )}
       </div>
