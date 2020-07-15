a382525dcd087ca41d8a6bffec19d3a6f485d55a
AmoDinho <u14284783@tuks.co.za>
2020-07-15 20:51:50 +0200

    max width of code set

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,16 +26,10 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <Footer />
-      </div>
+
+      <main>{children}</main>
+
+      <Footer />
     </>
   )
 }
--- src/components/navs/Footer.js
+++ src/components/navs/Footer.js
@@ -1,5 +1,9 @@
 import React from "react"
 
-const Footer = () => <footer>Hi</footer>
+const Footer = () => (
+  <footer className="border-bottom flex items-center justify-between">
+    Hi
+  </footer>
+)
 
 export default Footer
