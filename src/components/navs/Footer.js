d8c985333532eec527a1dd490ea3aaec92e81df1
AmoDinho <u14284783@tuks.co.za>
2020-07-15 21:22:31 +0200

    done

--- src/components/navs/Footer.js
+++ src/components/navs/Footer.js
@@ -1,8 +1,18 @@
 import React from "react"
-
+import BodyOne from "../typography/BodyOne"
 const Footer = () => (
-  <footer className="border-bottom flex items-center justify-between">
-    Hi
+  <footer className="border-bottom  mt-20 text-center">
+    <BodyOne className="mb-5">
+      Manufactured in 🇿🇦 by{" "}
+      <a
+        className="text-yellow mb-5"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/AmoDinho"
+      >
+        @PrinceAshburton{" "}
+      </a>
+    </BodyOne>
   </footer>
 )
 
