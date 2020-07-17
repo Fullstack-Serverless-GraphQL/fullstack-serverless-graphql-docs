d5b6f5d6f5824bb83d82f11b2a30a622fe58be03
AmoDinho <u14284783@tuks.co.za>
2020-07-17 20:46:51 +0200

    creating a new framework provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import TeckStack from "../components/landing/TechStack"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/landing/hero"
 import Chapters from "../components/chapters/Chapters"
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Hero />
-    <TeckStack />
-    <Chapters />
-  </Layout>
-)
+import { FrameworkContext } from "../frameworkContext"
+
+const IndexPage = () => {
+  const [framework, setFramework] = useState("vue")
+
+  const toggleFramework = () => {
+    setFramework(framework === "vue" ? "react" : "react")
+  }
+  return (
+    <FrameworkContext.Provider value={framework}>
+      <Layout>
+        <SEO title="Home" />
+        <Hero />
+        <TeckStack />
+        <Chapters />
+      </Layout>
+    </FrameworkContext.Provider>
+  )
+}
 
 export default IndexPage
