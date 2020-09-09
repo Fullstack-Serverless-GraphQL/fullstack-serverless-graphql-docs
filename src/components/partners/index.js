44b78576f8ad3ae9742225c1c99d6daf5652157a
AmoDinho <u14284783@tuks.co.za>
2020-09-09 21:22:46 +0200

    added the grid

--- src/components/partners/index.js
+++ src/components/partners/index.js
@@ -9,7 +9,7 @@ const Partners = () => {
         To make a bleeding edge appilcation you need amazing providers to make
         it imaginable and practical
       </HeadingThree>
-      <div className="gri grid-cols-3 gap-2">
+      <div className="grid grid-cols-3 gap-2">
         {constants.partners.map((p, i) => (
           <div className="flex flex-col" key={i}>
             <img src={p.logo} alt="partners" />
