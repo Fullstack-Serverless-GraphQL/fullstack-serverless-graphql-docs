7faaf1a155fb76ebe8f2d478799a5ef1e8f9adf2
AmoDinho <u14284783@tuks.co.za>
2020-09-09 21:09:19 +0200

    removed the data

--- src/components/partners/index.js
+++ src/components/partners/index.js
@@ -1,18 +1,5 @@
 import React from "react"
-import assets from "../../assets"
 const Partners = () => {
-  const partners = [
-    {
-      href: "test",
-      use: "Hosting of repos",
-      image: assets.github,
-    },
-    {
-      href: "test",
-      use: "Cloud provider",
-      image: assets.aws,
-    },
-  ]
   return (
     <>
       <div className="gri grid-cols-3 gap-2">
