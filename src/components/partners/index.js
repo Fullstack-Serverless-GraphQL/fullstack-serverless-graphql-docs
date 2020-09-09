195bfdbf95bf2c296bb999774f4f3ad1dc4866cf
AmoDinho <u14284783@tuks.co.za>
2020-09-09 20:51:21 +0200

    created partners page

--- src/components/partners/index.js
+++ src/components/partners/index.js
@@ -1,7 +1,30 @@
 import React from "react"
-
+import assets from "../../assets"
 const Partners = () => {
-  return <></>
+  const partners = [
+    {
+      href: "test",
+      use: "Hosting of repos",
+      image: assets.github,
+    },
+    {
+      href: "test",
+      use: "Cloud provider",
+      image: assets.aws,
+    },
+  ]
+  return (
+    <>
+      <div className="gri grid-cols-3 gap-2">
+        {partners.map((p, i) => (
+          <div className="flex flex-col" key={i}>
+            <img src={p.image} alt="partners" />
+            <p>{p.use}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Partners
