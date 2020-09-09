b20cc26797b84653d035190d66ef4ae9cb8dd8eb
AmoDinho <u14284783@tuks.co.za>
2020-09-09 21:21:41 +0200

    added heading

--- src/components/partners/index.js
+++ src/components/partners/index.js
@@ -1,8 +1,13 @@
 import React from "react"
 import constants from "../../constants"
+import HeadingThree from "../typography/HeadingThree"
 const Partners = () => {
   return (
     <>
+      <HeadingThree>
+        To make a bleeding edge appilcation you need amazing providers to make
+        it imaginable and practical
+      </HeadingThree>
       <div className="gri grid-cols-3 gap-2">
         {constants.partners.map((p, i) => (
           <div className="flex flex-col" key={i}>
