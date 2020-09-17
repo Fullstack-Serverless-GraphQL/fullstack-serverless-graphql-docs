9318c3bafa855dcd8a68bd632c63838cc9914636
AmoDinho <u14284783@tuks.co.za>
2020-09-17 20:53:11 +0200

    added landing page tracking

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Hero from "../components/landing/hero"
 import Chapters from "../components/chapters/Chapters"
 import { FrameworkContext } from "../frameworkContext"
 import Partners from "../components/partners"
+import constants from "../constants"
 const IndexPage = () => {
   const [framework, setFramework] = useState("vue")
 
@@ -13,6 +14,7 @@ const IndexPage = () => {
     setFramework(framework => (framework === "vue" ? "react" : "vue"))
   }
 
+  constants.track("App.LandingPage.View")
   console.log("ff-1", framework)
   return (
     <FrameworkContext.Provider value={{ framework, toggleFramework }}>
