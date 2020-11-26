812438ef6dfa9b5635175e3ea20cc298f0b7b1cb
AmoDinho <u14284783@tuks.co.za>
2020-11-26 21:51:18 +0200

    added copy for tech stack

--- src/components/landing/TechStack.js
+++ src/components/landing/TechStack.js
@@ -1,13 +1,20 @@
 import React from "react"
 import HeadingTwo from "../typography/HeadingTwo"
 import HeadingThree from "../typography/HeadingThree"
+import BodyOne from "../typography/BodyOne"
 import StackCard from "../cards/StackCard"
 import constants from "../../constants"
 const TechStack = () => (
   <>
     <div className="mt-12">
-      <HeadingTwo className="text-left"> Tech stack</HeadingTwo>
-      <HeadingThree className="text-blue text-left mb-5">Frontend</HeadingThree>
+      <HeadingTwo className="text-left font-extrabold"> Tech stack</HeadingTwo>
+      <HeadingThree className="text-blue  text-xl text-left mb-5 font-bold">
+        Frontend
+      </HeadingThree>
+      <BodyOne className="text-black  text-xl text-left mb-5 font-bold">
+        Using the Serverless Framework, we will be able to create GraphQL Lambda
+        powered by Apollo Server.
+      </BodyOne>
       <div className="grid grid-cols-3 gap-5 ml-10">
         {constants.techStack.map(t => (
           <StackCard url={t.logo} linkURL={t.url} Title={t.technology} />
