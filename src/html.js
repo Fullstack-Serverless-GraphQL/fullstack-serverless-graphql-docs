35b5d54e753834cb35f7348d6c7f7cfd03bc3c60
AmoDinho <u14284783@tuks.co.za>
2020-12-24 15:33:00 +0200

    added clairty

--- src/html.js
+++ src/html.js
@@ -12,6 +12,17 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+         (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "4ob3pdtjpj");
+         `,
+          }}
+        />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
