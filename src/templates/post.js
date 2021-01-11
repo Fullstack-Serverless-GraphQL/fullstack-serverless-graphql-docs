8059cc7c83472547276ada3481b5be7ff031cdd6
AmoDinho <u14284783@tuks.co.za>
2021-01-11 21:38:25 +0200

    added provider in post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -9,62 +9,61 @@ import SideChapterBox from "../components/chapters/SideChapterBox"
 import GithubButton from "../components/buttons/GithubButtton"
 import SEO from "../components/seo"
 import constants from "../constants"
-import { FrameworkContext } from "../frameworkContext"
+import FrameworkProvider from "../frameworkProvider"
 export default function Template({ data, pageContext }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   const { previous, next } = pageContext
   constants.track(`App.Post.View:${frontmatter.title}`)
-  const framwork = useContext(FrameworkContext)
-
-  console.log("FrameworkContext", framwork)
 
   return (
-    <Layout>
-      <SEO
-        title={frontmatter.title}
-        url={`https://fullstack-serverless-graphql.netlify.app/${frontmatter.path}`}
-      />
-      <div className="flex justify-center x:flex-col p-3">
-        <div className="blog-post">
-          <HeadingOne>{frontmatter.title}</HeadingOne>
-          <HeadingThree>{frontmatter.date}</HeadingThree>
-          <hr className="bg-grey" />
-          <div
-            className="markdown"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
+    <FrameworkProvider>
+      <Layout>
+        <SEO
+          title={frontmatter.title}
+          url={`https://fullstack-serverless-graphql.netlify.app/${frontmatter.path}`}
+        />
+        <div className="flex justify-center x:flex-col p-3">
+          <div className="blog-post">
+            <HeadingOne>{frontmatter.title}</HeadingOne>
+            <HeadingThree>{frontmatter.date}</HeadingThree>
+            <hr className="bg-grey" />
+            <div
+              className="markdown"
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </div>
+          <SideChapterBox collapse />
         </div>
-        <SideChapterBox collapse />
-      </div>
 
-      <div className="mt-10">
-        <ul className="flex justify-between">
-          <li className="">
-            {next && (
-              <Link to={next.node.frontmatter.path}>
-                <span className="flex flex-row">
-                  <img src={ArrowLeft} alt="arrow left" /> Previous
-                </span>
-              </Link>
-            )}
-          </li>
-          <GithubButton href="https://github.com/Fullstack-Serverless-GraphQL/fullstack-serverless-graphql-docs/issues/new?assignees=&labels=&template=help.md&title=">
-            Open an issue
-          </GithubButton>
+        <div className="mt-10">
+          <ul className="flex justify-between">
+            <li className="">
+              {next && (
+                <Link to={next.node.frontmatter.path}>
+                  <span className="flex flex-row">
+                    <img src={ArrowLeft} alt="arrow left" /> Previous
+                  </span>
+                </Link>
+              )}
+            </li>
+            <GithubButton href="https://github.com/Fullstack-Serverless-GraphQL/fullstack-serverless-graphql-docs/issues/new?assignees=&labels=&template=help.md&title=">
+              Open an issue
+            </GithubButton>
 
-          <li className="">
-            {previous && (
-              <Link to={previous.node.frontmatter.path}>
-                <span className="flex flex-row">
-                  Next <img src={ArrowRight} alt="arrow left" />
-                </span>
-              </Link>
-            )}
-          </li>
-        </ul>
-      </div>
-    </Layout>
+            <li className="">
+              {previous && (
+                <Link to={previous.node.frontmatter.path}>
+                  <span className="flex flex-row">
+                    Next <img src={ArrowRight} alt="arrow left" />
+                  </span>
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      </Layout>
+    </FrameworkProvider>
   )
 }
 export const pageQuery = graphql`
