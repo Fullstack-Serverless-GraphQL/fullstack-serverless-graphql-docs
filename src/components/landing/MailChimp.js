2a9e79989762ee20ec8280fca85ed29fea1c488e
AmoDinho <u14284783@tuks.co.za>
2021-01-19 21:49:21 +0200

    added red block button

--- src/components/landing/MailChimp.js
+++ src/components/landing/MailChimp.js
@@ -1,6 +1,7 @@
 import React from "react"
 import HeadingOne from "../typography/HeadingOne"
 import BodyOne from "../typography/BodyOne"
+import RedBlockButton from "../buttons/RedBlockButton"
 
 const MailChimp = () => {
   return (
@@ -8,6 +9,7 @@ const MailChimp = () => {
       <form>
         <div>
           <input type="email" placeholder="mazibuko@telsa.com" />
+          <RedBlockButton>Yes, I'm keen</RedBlockButton>
         </div>
       </form>
     </div>
