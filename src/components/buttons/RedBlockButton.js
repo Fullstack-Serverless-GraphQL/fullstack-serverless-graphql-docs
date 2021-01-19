223884073b8ce598a4c015fc0929e7b8ff34fda3
AmoDinho <u14284783@tuks.co.za>
2021-01-19 22:17:54 +0200

    added props

--- src/components/buttons/RedBlockButton.js
+++ src/components/buttons/RedBlockButton.js
@@ -1,15 +1,18 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-const RedBlockButton = ({ children, className }) => (
+const RedBlockButton = ({ children, className, onClick, isLoading }) => (
   <button
+    onClick={onClick}
     className={`${className} cursor-pointer bg-red-lightest text-red-darkest focus:outline-none font-display rounded-px px-16 py-2 hover:bg-transparent hover:border hover:border-red-darkest`}
   >
-    {children}
+    {isLoading ? "...loading" : children}
   </button>
 )
 
 RedBlockButton.propTypes = {
+  isLoading: PropTypes.bool,
+  onClick: PropTypes.func,
   children: PropTypes.string,
   className: PropTypes.string,
 }
