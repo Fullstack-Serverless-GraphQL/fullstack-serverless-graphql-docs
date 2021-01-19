78e66bddd142a811277092651c2a54075151690e
AmoDinho <u14284783@tuks.co.za>
2021-01-19 21:48:40 +0200

    created mailchimp component

---
+++ src/components/landing/MailChimp.js
@@ -0,0 +1,17 @@
+import React from "react"
+import HeadingOne from "../typography/HeadingOne"
+import BodyOne from "../typography/BodyOne"
+
+const MailChimp = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="email" placeholder="mazibuko@telsa.com" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default MailChimp
