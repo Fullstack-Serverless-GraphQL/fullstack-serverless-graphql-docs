72bcaa3300ca7e278b612c325867236029664861
AmoDinho <u14284783@tuks.co.za>
2021-01-19 22:00:34 +0200

    added copy

--- src/components/landing/MailChimp.js
+++ src/components/landing/MailChimp.js
@@ -1,13 +1,17 @@
 import React from "react"
-import HeadingOne from "../typography/HeadingOne"
 import BodyOne from "../typography/BodyOne"
 import RedBlockButton from "../buttons/RedBlockButton"
+import HeadingThree from "../typography/HeadingThree"
 
 const MailChimp = () => {
   return (
     <div>
       <form>
-        <div className="grid grid-rows-2 w-1/3">
+        <div className="grid grid-cols-1 w-1/3">
+          <HeadingThree>
+            Get notified when a PDF version of the guide drops and updates are
+            published.
+          </HeadingThree>
           <input
             type="email"
             placeholder="mazibuko@telsa.com"
