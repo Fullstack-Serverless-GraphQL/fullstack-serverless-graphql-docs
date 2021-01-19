7910cb362073c645f2722e85daf52e2bb9217833
AmoDinho <u14284783@tuks.co.za>
2021-01-19 22:19:48 +0200

    added ternary

--- src/components/landing/MailChimp.js
+++ src/components/landing/MailChimp.js
@@ -25,18 +25,24 @@ const MailChimp = () => {
     <div>
       <form>
         <div className="grid grid-cols-1 w-1/3">
-          <HeadingThree>
-            Get notified when a PDF version of the guide drops and updates are
-            published.
-          </HeadingThree>
-          <input
-            type="email"
-            placeholder="mazibuko@telsa.com"
-            className="p-2 mb-3"
-          />
-          <RedBlockButton onClick={() => addEmail()}>
-            Yes, I'm keen
-          </RedBlockButton>
+          {subscribed ? (
+            <p>Well done</p>
+          ) : (
+            <>
+              <HeadingThree>
+                Get notified when a PDF version of the guide drops and updates
+                are published.
+              </HeadingThree>
+              <input
+                type="email"
+                placeholder="mazibuko@telsa.com"
+                className="p-2 mb-3"
+              />
+              <RedBlockButton onClick={() => addEmail()} isLoading={isLoading}>
+                Yes, I'm keen
+              </RedBlockButton>
+            </>
+          )}
         </div>
       </form>
     </div>
